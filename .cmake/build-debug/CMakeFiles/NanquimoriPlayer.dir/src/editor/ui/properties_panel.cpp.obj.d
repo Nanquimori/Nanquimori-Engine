95bdc37b2b3f14c568143a4444125d36c583068c
Nanquimori <nanquimori@gmail.com>
2026-04-07 00:27:58 -0300

    Sync generated build artifacts

---
+++ .cmake/build-debug/CMakeFiles/NanquimoriPlayer.dir/src/editor/ui/properties_panel.cpp.obj.d
@@ -0,0 +1,55 @@
+CMakeFiles/NanquimoriPlayer.dir/src/editor/ui/properties_panel.cpp.obj: \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\properties_panel.cpp \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\properties_panel.h \
+ C:/raylib/raylib/src/raylib.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/vadefs.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/editor_layout.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/scene/outliner.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/app/application.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/scene/scene_manager.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/scene/outliner.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/scene/scene_camera.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/color_picker.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/text_input.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/drag_float_input.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/text_input.h \
+ C:/raylib/raylib/src/raymath.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/math.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/cmath \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/bits/requires_hosted.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/x86_64-w64-mingw32/bits/c++config.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/x86_64-w64-mingw32/bits/os_defines.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/x86_64-w64-mingw32/bits/cpu_defines.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/bits/cpp_type_traits.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/ext/type_traits.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/math.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/bits/std_abs.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdlib.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/limits.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/syslimits.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/limits.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/stdlib.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/malloc.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/mm_malloc.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/errno.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/bits/version.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/ui_style.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdio.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/string.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/cstdlib
---
+++ .cmake/build-debug/CMakeFiles/NanquimoriPlayer.dir/src/editor/ui/top_bar.cpp.obj.d
@@ -0,0 +1,42 @@
+CMakeFiles/NanquimoriPlayer.dir/src/editor/ui/top_bar.cpp.obj: \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\top_bar.cpp \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\top_bar.h \
+ C:/raylib/raylib/src/raylib.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/vadefs.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\export_dialog.h \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\file_explorer.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/text_input.h \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\help_panel.h \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\splash_screen.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/app/application.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/viewport/camera_controller.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/scene/outliner.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/editor/ui/editor_layout.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/scene/scene_camera.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/scene/outliner.h \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\properties_panel.h \
+ C:/Users/ANDREY~1/DOCUME~1/DOCUME~1/Raylib/NANQUI~1/src/assets/model_manager.h \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\ui_button.h \
+ C:\Users\Andrey\ Acelerion\Documents\Documentos\ Salvos\Raylib\Nanquimori\ Engine\src\editor\ui\ui_style.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/cstdio \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/x86_64-w64-mingw32/bits/c++config.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/x86_64-w64-mingw32/bits/os_defines.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/x86_64-w64-mingw32/bits/cpu_defines.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/stdio.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/corecrt.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/cstring \
+ C:/raylib/w64devkit/lib/gcc/x86_64-w64-mingw32/14.2.0/include/c++/bits/version.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/string.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/raylib/w64devkit/x86_64-w64-mingw32/include/sec_api/string_s.h
